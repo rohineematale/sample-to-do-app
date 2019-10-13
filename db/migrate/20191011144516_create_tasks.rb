841c84ffd70190c932040f6b45bba17d705f2aa9
RohineeMatale <rohineematale@gmail.com>
2019-10-14 03:37:51 +0530

    added constraint

--- db/migrate/20191011144516_create_tasks.rb
+++ db/migrate/20191011144516_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
       t.date :due_date
       t.integer :user_id
